98c8eb1b9f3515932d6a01d11ebfb8ee06495339
theSinner <amirajorloo@gmail.com>
2014-08-12 20:35:05 +0430

    fix setting

--- newblog/settings.py
+++ newblog/settings.py
@@ -90,6 +90,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.6/howto/static-files/
 
 STATIC_URL = '/static/'
-STATICFILES_DIRS = ('/Users/Amir/Projects/newblog/newblog/static',    )
+STATICFILES_DIRS = (join(BASE_DIR, 'static'),    )
 
 
